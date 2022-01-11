3f515e04878111cef6bc0b31aefa4e19e5f3b80f
Gabriel Frasson <bielfc27@gmail.com>
2022-01-11 20:23:37 -0300

    Extraindo contornos de todas as imagens de uma pasta

--- draw_contours.py
+++ draw_contours.py
@@ -1,9 +1,12 @@
 import cv2 as cv
 import numpy as np
-from os import path
+from os import path, walk
 
 
 def draw_countours(image, contornos, color: tuple = (0, 0, 255)):
+    if len(contornos[0]) == 0:
+        return image
+    
     for contorno in contornos:
         for point in contorno:
             pts = []
@@ -29,25 +32,23 @@ if __name__ == '__main__':
     image_dir_path = path.join(path.curdir, 'images')
     thresh_dir_path = path.join(path.curdir, 'threshold_images')
     contoured_image_dir_path = path.join(path.curdir, 'contoured_images')
-    subdir_path = path.join('MotoC', 'Coffee')
+    subdir_path = path.join('MotoC', 'Orange')
 
-    # Nome da imagem
-    image_name = 'Coffee_1_pat_1cm_dist_30cm_MotoC'
+    for root, dirs, files in walk(path.join(image_dir_path, subdir_path)):
+        for file in files:
+            # Nome da imagem
+            image_name = file.split('.')[0]
+            print(image_name)
 
-    # Leitura da imagem
-    thresh = cv.imread(path.join(thresh_dir_path, subdir_path, image_name + '_mask.png'), cv.IMREAD_UNCHANGED)
-    image = cv.imread(path.join(image_dir_path, subdir_path, image_name + '.jpg'), cv.IMREAD_UNCHANGED)
+            # Leitura da imagem
+            thresh = cv.imread(path.join(thresh_dir_path, subdir_path, image_name + '_mask.png'), cv.IMREAD_UNCHANGED)
+            image = cv.imread(path.join(image_dir_path, subdir_path, image_name + '.jpg'), cv.IMREAD_UNCHANGED)
 
-    # Encontrando contornos
-    contours = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+            # Encontrando contornos
+            contours = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
-    # Desenhando contornos na imagem original
-    contoured_image = draw_countours(image, contours)
+            # Desenhando contornos na imagem original
+            contoured_image = draw_countours(image, contours)
 
-    # Preview
-    # cv.namedWindow("Window", cv.WINDOW_NORMAL);
-    # cv.imshow("Window", image);
-    # cv.waitKey(0)
-
-    # Salvando imagem contornada
-    cv.imwrite(path.join(contoured_image_dir_path, subdir_path, image_name + '_contour' + '.png'), contoured_image)
+            # Salvando imagem contornada
+            cv.imwrite(path.join(contoured_image_dir_path, subdir_path, image_name + '_contour' + '.png'), contoured_image)
